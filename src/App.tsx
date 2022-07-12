d36832a3a304381a439a4a393952b75c9fb7a4f3
Mkhs <jonbecher@microagenetworks.com>
2022-07-12 12:05:37 -0400

    Created sample input textareas for input values and a sample label

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,14 @@ function App() {
         <div className="w-full p-6 bg-blue-100 dark:bg-slate-600">
           <div className="w-48 mx-auto pt-6 border-b-2 border-orange-500 text-center text-2xl text-blue-700 dark:text-white border-red-500">Mortgage Calculator</div>
           <div className="p-2 text-center text-lg text-gray-700 dark:text-white">Lightweight API based mortgage calculator.</div>
-          <div className="flex justify-center flex-wrap p-10">
-            <div className="relative w-48 h-64 m-5 bg-white shadow-lg dark:bg-slate-700">
-              <div className=" w-48 h-20 bg-orange-500 dark:bg-red-500">
-                <i className="flex items-center fas fa-bezier-curve fa-3x mx-auto text-white"></i>
-              </div>
-              <p className="mx-2 py-2 border-b-2 text-center text-gray-700 font-semibold uppercase dark:text-white">TESTING</p>
-              <p className="p-2 text-sm text-gray-700 dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ac est massa.</p>
-              <div className="absolute right-0 bottom-0 w-8 h-8 bg-gray-300 hover:bg-orange-300 text-center cursor-pointer">
-                <i className="fas fa-chevron-right mt-2 text-orange-500"></i>
-              </div>
-            </div>
+          <label htmlFor="input1">testing</label>  
+            <div className="flex justify-between flex-wrap p-2">
+            <input id="input1" className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
+            <input id="input2"className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
+            <input className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
+            <input className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
+            <input className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
+            <input className="m-2 text-center hover:bg-blue-200" placeholder="test"></input>
           </div>
         </div>
         <div className="flex flex-row p-3 justify-center">

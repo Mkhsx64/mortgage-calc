3481df142cdb1435ca7cb20adbc1a1be0614182e
Mkhs <jonbecher@microagenetworks.com>
2022-07-12 00:24:47 -0400

    messing around with tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <main className="bg-white dark:bg-slate-800">
-
-    </main>
+    <>
+      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+            stuff
+      </figure>
+    </>
   );
 }
 

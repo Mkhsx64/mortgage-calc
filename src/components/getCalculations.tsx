e8936b37c78fa280ba2948bad11bc774d47ee0c4
Mkhs <jonbecher@microagenetworks.com>
2022-07-14 02:22:23 -0400

    add components folder to git

---
+++ src/components/getCalculations.tsx
@@ -0,0 +1,11 @@
+const url = 'https://api.api-ninjas.com/v1/mortgagecalculator?'
+
+
+export function callMortgageAPI(mortgageData: object) {
+    for (let k in mortgageData) {
+        //let value = mortgageData[k];
+    }
+    return;
+}
+
+
